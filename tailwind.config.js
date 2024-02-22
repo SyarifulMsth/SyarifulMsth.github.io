816239db20d440f563b0d0e62a5c1607ba224f6d
SyarifulMsth <syariful1605@gmail.com>
2024-02-22 19:57:06 +0700

    :new:

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,12 @@ module.exports = {
     },
     extend: {
       listStyleImage: {
-        redBullet: 'url("/src/bullet/Red/icons16.png")',
-        greenBullet: 'url("/src/bullet/Green/icons16.png")',
+        redBullet: 'url("/dist/bullet/Red/icons16.png")',
+        greenBullet: 'url("/dist/bullet/Green/icons16.png")',
       },
       boxShadow: {
         'shadow2': 'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px',
+        'shadow8': 'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px',
         'shadow28': 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
         'shadow84': 'rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px',
         'shadow85': 'rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset',
