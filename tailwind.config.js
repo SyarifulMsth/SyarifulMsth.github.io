dabcd353d895275c2c233877992a70f89486e6c4
SyarifulMsth <syariful1605@gmail.com>
2024-02-22 10:45:19 +0700

    :new:

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       Forum: ['forum', 'serif'],
       Fredericka: ['Fredericka the Great', 'serif'],
       Nunito: ['Nunito Sans', 'sans-serif'],
+      Poppins: ['Poppins', 'sans-serif'],
     },
     extend: {
       listStyleImage: {
@@ -14,10 +15,11 @@ module.exports = {
         greenBullet: 'url("/src/bullet/Green/icons16.png")',
       },
       boxShadow: {
-        'shadow2': 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px',
+        'shadow2': 'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px',
         'shadow28': 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
         'shadow84': 'rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px',
-        'shadow85': 'rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset'
+        'shadow85': 'rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset',
+        'shadow89': 'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
       },
       // colors: {
       //   red: {
